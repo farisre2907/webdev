b331fb06b0e51e052556f1a2ae9ad2256df27980
farisrifqyy <faris.re2907@gmail.com>
2026-06-14 03:03:36 +0700

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/webdev/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
